76ce91ba9bdb78579c13fcd264f52ff007d0bdf8
PluZ Kongsompong <geruby85@gmail.com>
2020-04-11 01:23:56 +0700

    Change Wrapper to Luxon ⏱

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const core = require('@actions/core');
-const moment = require('moment');
-const timezone = require('moment-timezone');
-
+const {DateTime} = require('luxon');
 
 function run() {
-  moment.locale('th');
-  let th_format = 'DD MMM YY เวลา HH:mm';
-  let th_zone = timezone.tz('Asia/Bangkok').add(543, 'years');
   try {
-    core.setOutput('thaiTime', th_zone.format(th_format));
+    let th_format = 'dd LLL yy เวลา HH:mm';
+    let t_time = DateTime.local().plus({ years: 543 }).setZone('Asia/Bangkok').toFormat(th_format,{ locale: 'th', numberingSystem: 'thai'});
+    core.setOutput('thaiTime', t_time);
   } 
   catch (error) {
     core.setFailed(error.message);
