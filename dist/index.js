6a3a2dc7bbc733891e1e66103a8d0dae26bade71
PluZ85 <geruby85@gmail.com>
2020-04-17 03:23:42 +0700

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -58,8 +58,8 @@ const {DateTime} = __webpack_require__(317);
 
 function run() {
   try {
-    let th_format = 'EEE dd MMM yy เวลา HH:mm';
-    let t_time = DateTime.local().setZone('Asia/Bangkok').plus({years: 543}).minus({days: 2}).toFormat(th_format);
+    let th_format = 'dd MMM yy เวลา HH:mm';
+    let t_time = DateTime.local().setZone('Asia/Bangkok').plus({years: 543}).toFormat(th_format);
     core.setOutput('thaiTime', t_time);
   } 
   catch (error) {
